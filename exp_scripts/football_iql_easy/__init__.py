136811bbf5449e9e8bd6e85bb29fc25a23ebf0a9
Cong Lu <cong.lu@balliol.ox.ac.uk>
2020-04-10 03:02:23 +0100

    easy scens

--- exp_scripts/football_iql_easy/__init__.py
+++ exp_scripts/football_iql_easy/__init__.py
@@ -1,5 +1,6 @@
 import copy
 
+
 def extend_param_dicts(param_dicts, shared_params, exp_params, repeats=1):
     # copy
     result = copy.deepcopy(shared_params)
@@ -10,13 +11,14 @@ def extend_param_dicts(param_dicts, shared_params, exp_params, repeats=1):
         param_dicts.append(copy.deepcopy(result))
     return param_dicts
 
+
 server_list = [("gimli", [1], 6)]
 
-label = "gfootball_iql"
+label = "gfootball_iql_easy"
 config = "iql"
 env_config = "gfootball"
 
-n_repeat = 5
+n_repeat = 1
 
 param_dicts = []
 
@@ -24,17 +26,16 @@ shared_params = {
     "t_max": 2000000,
     "epsilon_anneal_time": 250000,
     "buffer_size": 5000,
-    "env_args.episode_limit": 400,
+    "env_args.episode_limit": 1000,
 }
 
-
 for map_name in ["academy_pass_and_shoot_with_keeper",
-                 "academy_run_pass_and_shoot_with_keeper", 
+                 "academy_run_pass_and_shoot_with_keeper",
                  "academy_3_vs_1_with_keeper",
                  "academy_counterattack_easy"]:
     extend_param_dicts(param_dicts, shared_params,
-        {
-            "name": "iql__{}".format(map_name),
-            "env_args.scenario": map_name
-        },
-        repeats=1)
+                       {
+                           "name": "iql__{}".format(map_name),
+                           "env_args.scenario": map_name
+                       },
+                       repeats=1)
