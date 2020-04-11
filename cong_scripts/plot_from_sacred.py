71b95f9ce8252f86e4a00254988f41fb4946bf79
Cong Lu <cong.lu@balliol.ox.ac.uk>
2020-04-11 03:13:44 +0100

    scripts

--- cong_scripts/plot_from_sacred.py
+++ cong_scripts/plot_from_sacred.py
@@ -3,32 +3,30 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 if __name__ == '__main__':
     for metric in ['return_mean', 'score_reward_mean']:
         metric_t = '{0}_T'.format(metric)
-        for exp_name in ['iql__academy_pass_and_shoot_with_keeper', 'iql__academy_run_pass_and_shoot_with_keeper', 'iql__academy_counterattack_easy', 'iql__academy_3_vs_1_with_keeper']:
+        data_dict = defaultdict(list)
+
+        for index in range(88821, 88844):
+            with open('/Users/conglu/Docker Repos/pymarl/results/sacred/{0}/config.json'.format(index)) as json_file:
+                data = json.load(json_file)
+                name = data['name']
+                print(name)
+
+            with open('/Users/conglu/Docker Repos/pymarl/results/sacred/{0}/info.json'.format(index)) as json_file:
+                data = json.load(json_file)
+                ys = data[metric]
+                xs = data[metric_t]
+                data_dict[name].append((xs, ys))
+
+        for key, points in data_dict.items():
             plt.clf()
-
-            points = []
-
-            for index in range(1, 14):
-                with open('/Users/conglu/Docker Repos/pymarl/results/sacred/{0}/config.json'.format(index)) as json_file:
-                    data = json.load(json_file)
-                    name = data['name']
-
-                with open('/Users/conglu/Docker Repos/pymarl/results/sacred/{0}/info.json'.format(index)) as json_file:
-                    data = json.load(json_file)
-                    ys = data[metric]
-                    xs = data[metric_t]
-                    if exp_name in name:
-                        points.append((xs, ys))
-
             for xs, ys in points:
-                print(xs)
                 plt.plot(xs, ys, label=name)
-
             plt.xlabel('T env')
             plt.ylabel(metric)
             plt.legend()
-            # plt.savefig("{0}_{1}.png".format(metric, exp_name))
+            plt.savefig("{0}_{1}.png".format(metric, key))
